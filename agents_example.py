9bac425cfba05d24989698ac93102cfcdd36ebf6
Dasith Wijes <git@dasith.me>
2024-01-23 15:19:56 +0000

    fix types

--- agents_example.py
+++ agents_example.py
@@ -18,14 +18,13 @@ Run `python3 ./store_and_stock_app.py` to start the required store and stock ser
 
 import streamlit as st
 from termcolor import colored
-from langchain_experimental.plan_and_execute import (
-    PlanAndExecute,
-    load_agent_executor,
-    load_chat_planner,
-)
+
+from langchain import hub
+from langchain.agents import AgentExecutor, create_json_chat_agent
+
 from langchain import SerpAPIWrapper
 from langchain.agents.tools import Tool
-from langchain.agents import initialize_agent, AgentType
+#from langchain.agents import initialize_agent, AgentType
 from langchain import LLMMathChain
 from langchain.memory import ConversationBufferMemory
 from langchain.prompts import MessagesPlaceholder
@@ -35,7 +34,6 @@ import json
 
 from catalog import get_catalog
 
-usePlanAndExecuteAgentType = True
 useBuiltInSearchAndCalculatorTools = False
 useUserInputTool = True
 
@@ -47,10 +45,7 @@ user_input_history = st.session_state['user_input_history']
 st.set_page_config(page_title="🥼🧪 Experimenting With Langchain Agents And \"Knowledge Provider Mesh\"")
 st.title("🥼🧪 Experimenting With Langchain Agents And \"Knowledge Provider Mesh\"")
 
-if usePlanAndExecuteAgentType:
-    st.info("Hi. This agent is using PlanAndExecute agent type. Type your query and sit submit.")
-else:
-    st.info("Hi. This agent is using STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION agent type. Type your query and sit submit.")
+st.info("Hi. This agent is using JSON Chat Agent type. Type your query and sit submit.")
 
 
 def get_user_input(query: str) -> str:
@@ -97,7 +92,7 @@ def setup_agent():
             )
         ]
 
-    if useUserInputTool and usePlanAndExecuteAgentType:
+    if useUserInputTool:
         # add user input tool for plan and execute agent as it doesn't support memory yet
         tools.append(
             Tool(name="UserInput",
@@ -122,29 +117,23 @@ def setup_agent():
     knowledge_tools = [service.get_tool() for service in get_catalog()]
     tools.extend(knowledge_tools)
 
-    if usePlanAndExecuteAgentType:
-        # plan and execute - https://python.langchain.com/docs/modules/agents/agent_types/plan_and_execute
-        planner = load_chat_planner(llm)
-        executor = load_agent_executor(llm, tools, verbose=True)
-        agent = PlanAndExecute(planner=planner, executor=executor, verbose=True)
-    else :
-        # structured tool - https://python.langchain.com/docs/modules/agents/agent_types/structured_chat.html
-        agent = initialize_agent(tools, llm, agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION,
-                                memory=memory,
-                                agent_kwargs={
-                                    "memory_prompts": [chat_history],
-                                    "input_variables": ["input", "agent_scratchpad", "chat_history"]
-                                },
-                                verbose=True)
+    # json chat agent https://python.langchain.com/docs/modules/agents/agent_types/json_agent
+    prompt = hub.pull("hwchase17/react-chat-json")
+    agent = create_json_chat_agent(llm, tools, prompt)
 
-    return agent
+    # Create an agent executor by passing in the agent and tools
+    agent_executor = AgentExecutor(
+        agent=agent, tools=tools, verbose=True, handle_parsing_errors=True
+    )
 
-agent = setup_agent()
+    return agent_executor
+
+agent_executor = setup_agent()
 
 def generate_response(input_text):
 
     with st.spinner(text="Generating... Please check the agent backend to see if it requires further user input."):
-        response = agent({"input": input_text})
+        response = agent_executor.invoke({"input": input_text})
 
         st.info(response["output"], icon="🤖")
 
